fd9f70add3596921077f327bc8966684b2e0f27a
Open-Cor3 <re0915618@gmail.com>
2025-01-01 17:44:13 +0100

    Update ESPApi.lua

--- ESP/ESPApi.lua
+++ ESP/ESPApi.lua
@@ -1,18 +1,18 @@
 local ESP = {
-    Enabled = true,
-    Tracers = true,
-    Box = true,
-    Skeleton = true,
-    HealthBar = true,
+    Enabled = false,
+    Tracers = false,
+    Box = false,
+    Skeleton = false,
+    HealthBar = false,
     TeamCheck = false,
-    TeamColor = true,
+    TeamColor = false,
     TracerColor = Color3.fromRGB(255, 0, 0),
     BoxColor = Color3.fromRGB(255, 0, 0),
     HealthBarColor = Color3.fromRGB(0, 255, 0),
     BoxThickness = 2,
     TracerThickness = 1,
-    ShowHealthBar = true,
-    ShowSkeleton = true
+    ShowHealthBar = false,
+    ShowSkeleton = false
 }
 
 local Players = game:GetService("Players")
@@ -32,6 +32,8 @@ local function RemoveESP(plr)
 end
 
 local function UpdateESP(plr)
+    if not ESP.Enabled then return end
+
     local function DrawESP()
         if plr.Character and plr.Character:FindFirstChild("HumanoidRootPart") then
             local rootPos, onScreen = Camera:WorldToViewportPoint(plr.Character.HumanoidRootPart.Position)
@@ -50,7 +52,7 @@ local function UpdateESP(plr)
                     box.PointD = Vector2.new(humPos.X + distance, humPos.Y + distance * 2)
                     box.Thickness = ESP.BoxThickness
                     box.Color = ESP.BoxColor
-                    box.Visible = true
+                    box.Visible = ESP.Enabled
                     table.insert(espObjects, box)
                 end
 
@@ -60,7 +62,7 @@ local function UpdateESP(plr)
                     tracer.To = Vector2.new(humPos.X, humPos.Y + distance * 2)
                     tracer.Thickness = ESP.TracerThickness
                     tracer.Color = ESP.TracerColor
-                    tracer.Visible = true
+                    tracer.Visible = ESP.Enabled
                     table.insert(espObjects, tracer)
                 end
 
@@ -71,7 +73,7 @@ local function UpdateESP(plr)
                     healthbar.To = Vector2.new(humPos.X - distance - 4, humPos.Y + distance * 2 - healthHeight)
                     healthbar.Thickness = 2
                     healthbar.Color = ESP.HealthBarColor
-                    healthbar.Visible = true
+                    healthbar.Visible = ESP.Enabled
                     table.insert(espObjects, healthbar)
                 end
 
@@ -82,7 +84,7 @@ local function UpdateESP(plr)
                     skeleton.head.To = Vector2.new(humPos.X, humPos.Y + distance * 2)
                     skeleton.head.Thickness = 2
                     skeleton.head.Color = ESP.BoxColor
-                    skeleton.head.Visible = true
+                    skeleton.head.Visible = ESP.Enabled
                     table.insert(espObjects, skeleton.head)
 
                     skeleton.torso = Drawing.new("Line")
@@ -90,7 +92,7 @@ local function UpdateESP(plr)
                     skeleton.torso.To = Vector2.new(humPos.X, humPos.Y + distance * 1.5)
                     skeleton.torso.Thickness = 2
                     skeleton.torso.Color = ESP.BoxColor
-                    skeleton.torso.Visible = true
+                    skeleton.torso.Visible = ESP.Enabled
                     table.insert(espObjects, skeleton.torso)
 
                     skeleton.leftLeg = Drawing.new("Line")
@@ -98,7 +100,7 @@ local function UpdateESP(plr)
                     skeleton.leftLeg.To = Vector2.new(humPos.X - 5, humPos.Y + distance * 2)
                     skeleton.leftLeg.Thickness = 2
                     skeleton.leftLeg.Color = ESP.BoxColor
-                    skeleton.leftLeg.Visible = true
+                    skeleton.leftLeg.Visible = ESP.Enabled
                     table.insert(espObjects, skeleton.leftLeg)
 
                     skeleton.rightLeg = Drawing.new("Line")
@@ -106,7 +108,7 @@ local function UpdateESP(plr)
                     skeleton.rightLeg.To = Vector2.new(humPos.X + 5, humPos.Y + distance * 2)
                     skeleton.rightLeg.Thickness = 2
                     skeleton.rightLeg.Color = ESP.BoxColor
-                    skeleton.rightLeg.Visible = true
+                    skeleton.rightLeg.Visible = ESP.Enabled
                     table.insert(espObjects, skeleton.rightLeg)
                 end
 
@@ -118,6 +120,15 @@ local function UpdateESP(plr)
     coroutine.wrap(DrawESP)()
 end
 
+local function ToggleESP()
+    ESP.Enabled = not ESP.Enabled
+    for _, plr in pairs(Players:GetPlayers()) do
+        if plr.Name ~= Players.LocalPlayer.Name then
+            RemoveESP(plr)
+            UpdateESP(plr)
+        end
+    end
+end
 
 Players.PlayerAdded:Connect(function(newPlayer)
     if newPlayer.Name ~= Players.LocalPlayer.Name then
@@ -136,6 +147,5 @@ for _, v in pairs(Players:GetPlayers()) do
         UpdateESP(v)
     end
 end
-
 
 return ESP
